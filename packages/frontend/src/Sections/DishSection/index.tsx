37c68efdd5633818d4b0df6824173f148048a015
Nelson Tejeda <47905303+NelsonTejeda@users.noreply.github.com>
2019-11-09 18:42:19 -0500

    dish section (#22)
    * these are the data types. Don't know if I did them right.
    
    * data types
    
    * fixed errors on data types
    
    * made basic dish section

--- packages/frontend/src/Sections/DishSection/index.tsx
+++ packages/frontend/src/Sections/DishSection/index.tsx
@@ -1,14 +1,49 @@
-import React from 'react';
-import {useParams} from 'react-router-dom';
+import React from "react";
+import { useParams } from "react-router-dom";
+import "./index.scss";
 
-
-const DishSection:React.FC = () =>{
-    let {dish,venue}=useParams();
-    return(
-        <div>
-            <h1> {dish} </h1>
-            <p> from {venue}</p>
+const DishSection: React.FC = () => {
+  let { dish, venue } = useParams();
+  return (
+    <div>
+      <div className="header">
+        <h1>{venue}</h1>
+      </div>
+      <div className="grid">
+        <div className="imgbox">
+          <img
+            src="http://www.pngplay.com/wp-content/uploads/1/Bread-Sandwich-Transparent-Background.png"
+            alt="oh poop"
+          />
         </div>
-    )
+        <div className="textbox">
+          <div className="dishName">
+            <h1>{dish}</h1>
+          </div>
+          <h3>Calories: XXX</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta est
+            possimus, culpa necessitatibus excepturi, optio aut atque repellat
+            beatae iusto dolorum aspernatur qui ipsum illum non magnam
+            assumenda, nemo dolores! Lorem ipsum dolor, sit amet consectetur
+            adipisicing elit. Hic unde molestias voluptas velit natus sint at
+            deleniti officia maxime perspiciatis quae, deserunt, nobis cumque
+            dolores neque dolor iste ipsa ducimus!
+          </p>
+        </div>
+      </div>
+      <div>
+        <h1>Ingredients</h1>
+        <div>
+
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex nulla
+          optio fuga voluptate eius quisquam. Qui porro necessitatibus labore
+          earum officiis officia, sint nemo sapiente eligendi incidunt cum odio.
+          Ut?
+
+        </div>
+      </div>
+    </div>
+  );
 };
 export default DishSection;
