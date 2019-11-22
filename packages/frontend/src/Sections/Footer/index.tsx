422f429f27d913ff27a4473bb6a814a5410b5278
astrahl008 <astra008@plattsburgh.edu>
2019-11-21 19:43:01 -0500

    Created footer section.

---
+++ packages/frontend/src/Sections/Footer/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './index.scss';
+const FooterSection: React.FC = ()=>{
+    return(
+        <div className="footer">
+
+        </div>
+    )
+}
+
+export default FooterSection;
