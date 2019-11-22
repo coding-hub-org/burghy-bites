16ca7fb37fe496e78fdb735267d0cc2271eaf446
winkhanh <giakhanhnk2000@gmail.com>
2019-11-21 19:11:02 -0500

    merge upstream to origin

--- packages/backend/src/models/Venue.ts
+++ packages/backend/src/models/Venue.ts
@@ -4,10 +4,6 @@ export const VenuesSchema = new Schema({
   name: {
     type: String
   },
-<<<<<<< HEAD
-=======
-
->>>>>>> a972c76818d834d9a3bddb6c4d58ca7e32a5a693
   dishes: [Dish.schema],
   hours: {
     type: String
