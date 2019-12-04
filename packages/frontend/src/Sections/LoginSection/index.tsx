a0100700b797ca8282dfa23605098bcbeb648872
Adora-A <aasin001@plattsburgh.edu>
2019-12-03 23:44:08 -0500

    fix placements, improve css

--- packages/frontend/src/Sections/LoginSection/index.tsx
+++ packages/frontend/src/Sections/LoginSection/index.tsx
@@ -2,27 +2,31 @@ import React from 'react';
 import './index.scss';
 const LoginSection:React.FC = () =>{
     return(
-        <div>
-            <h1 className="login-header">LOGIN</h1>
-            <div>                                        
-                <h2>Welcome back! Did you </h2><a href="/">forget your password?</a>
+<body className = "bg-img">
+    <form action = "/action_page.php" method="post">
+        <div className="login-box">
+            <h1 className="txt-color">Login</h1>
+            <br></br>
+            <div className="input_container">
+                <input placeholder="Username" type="text" name="Username" className='input-field'/>
             </div>
+            <br></br>
+            <div className="input_container">
+                <input placeholder="Password" type="password" name="Password" className='input-field'/>
+            </div>
+            <br></br>
             <div>
-                <input type="text"  placeholder="Username"/>
+                <button type="submit" value="Login" id='input_submit' className="input_submit">Login</button>
             </div>
+            <br></br>
             <div>
-                <input type="password" placeholder="Password"/>
-            </div>
-            
-        
-            <div >
-                <input type="checkbox" />
-                <label >Remember me</label>
-            </div>
-        
-
-            <button>Login</button>
+            <span className="txt-color">Forgot <a className="links" href="#"> Username / Password ?</a></span> 
+            </div>  
         </div>
+
+
+    </form>
+</body>
     )
 };
 
